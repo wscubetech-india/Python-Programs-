084433531f162b884f2881d75959b26d8f6c368b
wscubetech-india <110541254+wscubetech-india@users.noreply.github.com>
2022-08-03 18:02:26 +0530

    Add files via upload

---
+++ AddTwoNums.py
@@ -0,0 +1,16 @@
+#Solution-1 (with pre-defined Variables)
+
+# num1 = 30
+# num2 = 40
+# sum = num1+num2
+# print ("the sum of given two number is ", sum)
+
+#Solution-2 (with user-inputs)
+
+num1 = float(input("enter a number here: "))
+num2 = float(input("enter another number here: "))
+
+sum = num1 + num2
+
+print ("the sum of the provided two numbers is", sum)
+
